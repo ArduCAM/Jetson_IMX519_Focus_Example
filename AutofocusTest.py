de6bba65fc0805dbea4f055799547ba8ad99662a
liktenis <destiny06666@gmail.com>
2021-11-26 11:26:52 +0800

    Add cmdline args i2c-bus and verbose to AutofocusTest.py

--- AutofocusTest.py
+++ AutofocusTest.py
@@ -2,6 +2,7 @@ import time
 import signal
 import cv2
 import threading
+import argparse
 from JetsonCamera import Camera
 from Focuser import Focuser
 from Autofocus import FocusState, doFocus
@@ -14,12 +15,24 @@ def sigint_handler(signum, frame):
 signal.signal(signal.SIGINT, sigint_handler)
 signal.signal(signal.SIGTERM, sigint_handler)
 
+def parse_cmdline():
+	parser = argparse.ArgumentParser(description='Arducam IMX519 Autofocus Demo.')
+
+	parser.add_argument('-i', '--i2c-bus', type=int, nargs=None, required=True,
+						help='Set i2c bus, for A02 is 6, for B01 is 7 or 8, for Jetson Xavier NX it is 9 and 10.')
+
+	parser.add_argument('-v', '--verbose', action="store_true", help='Print debug info.')
+
+	return parser.parse_args()
+
 if __name__ == "__main__":
+	args = parse_cmdline()
 	camera = Camera()
-	focuser = Focuser(7)
+	focuser = Focuser(args.i2c_bus)
+	focuser.verbose = args.verbose
 
 	focusState = FocusState()
-	# focusState.verbose = True
+	focusState.verbose = args.verbose
 	doFocus(camera, focuser, focusState)
 
 	start = time.time()
